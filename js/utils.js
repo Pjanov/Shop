a192a1fbb6a2bfe68cde40f3adc295cb0bde1c16
pjanov <pjanov84@mail.ru>
2025-04-10 01:51:17 +0300

    commit

--- js/utils.js
+++ js/utils.js
@@ -1,14 +1,23 @@
 function getCartItems() {
-    return JSON.parse(localStorage.getItem('cart')) || [];
+  return JSON.parse(localStorage.getItem('cart')) || [];
+}
+
+function saveCartItems(items) {
+  localStorage.setItem('cart', JSON.stringify(items));
+}
+
+function addToCart(id) {
+  let cart = getCartItems();
+  cart.push(id);
+  saveCartItems(cart);
+  alert('Добавлено в корзину');
+}
+
+<script>
+  const cart = JSON.parse(localStorage.getItem('cart')) || [];
+  const count = cart.length;
+  const cartCount = document.getElementById('cart-count');
+  if (cartCount) {
+    cartCount.innerText = count;
   }
-  
-  function saveCartItems(items) {
-    localStorage.setItem('cart', JSON.stringify(items));
-  }
-  
-  function addToCart(id) {
-    let cart = getCartItems();
-    cart.push(id);
-    saveCartItems(cart);
-    alert('Добавлено в корзину');
-  }
+</script>
